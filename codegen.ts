3d269fef5718374e9c2cd144b22c01eb0e1e5839
kobinarth-panchalingam <kobinarth22@gmail.com>
2024-05-27 01:14:50 +0530

    Add required dependencies

---
+++ codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+	schema: 'http://localhost:4000',
+	documents: [ 'src/**/*.tsx' ],
+	generates: {
+		'./src/__generated__/': {
+			preset: 'client',
+			presetConfig: {
+				gqlTagName: 'gql'
+			}
+		}
+	},
+	ignoreNoDocuments: true
+};
