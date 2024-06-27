2e8066828df56f52d920e4d4f411acb6a271b474
Kobinarth Panchalingam <kobinarth22@gmail.com>
2024-06-27 10:36:52 +0530

    Add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
 	plugins: [ react() ],
-	envPrefix: 'VITE_'
+	envPrefix: 'VITE_',
+	resolve: {
+		alias: {
+			'@': path .resolve( __dirname, './src' ),
+			'@api': path .resolve( __dirname, './src/api' ),
+			'@utils': path .resolve( __dirname, './src/utils' ),
+			'@constants': path .resolve( __dirname, './src/constants' ),
+			'@contexts': path .resolve( __dirname, './src/contexts' )
+		}
+	}
 } );
